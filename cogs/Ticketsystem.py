0c647ba115b57e66f8c70ce642b6f396d81526ed
lxstkrieger <oliverpetermorawski1.de@gmail.com>
2024-03-02 09:34:31 +0100

    Ticketsystem.py was updated

--- cogs/Ticketsystem.py
+++ cogs/Ticketsystem.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.commands import slash_command
+from discord.ui import button
 import logging
 import sqlite3
 import os
@@ -21,6 +22,41 @@ cursor.execute('''
           )
           ''')
 conn.commit()
+
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__()
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------")
+
+
+class Confirm(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    # When the confirm button is pressed, set the inner value
+    # to `True` and stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
+    async def confirm_callback(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        await interaction.response.send_message("Confirming", ephemeral=True)
+        self.value = True
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value to `False`.
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.grey)
+    async def cancel_callback(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        await interaction.response.send_message("Cancelling", ephemeral=True)
+        self.value = False
+        self.stop()
 
 
 class Ticketsystem(commands.Cog):
@@ -84,7 +120,7 @@ class Ticketsystem(commands.Cog):
             logging.error(f'An error occurred in {self.__class__.__name__}: {e}', exc_info=True)
 
     @slash_command(description="Close a Ticket(Only Team Member can Use This!)")
-    async def closeticket(self, ctx):
+    async def closeticket(self, ctx, ticket_name: commands.TextChannelConverter):
         try:
             # Check permissions in the database before closing the ticket
             cursor.execute('''
@@ -95,10 +131,21 @@ class Ticketsystem(commands.Cog):
             result = cursor.fetchone()
 
             if result:
-                await ctx.channel.delete()
+                # We create the View and assign it to a variable so that we can wait for it later.
+                view = Confirm()
+                await ctx.send("Do you want to continue?", view=view)
+                # Wait for the View to stop listening for input...
+                await view.wait()
+                if view.value is None:
+                    await ctx.respond("Timed out...")
+                elif view.value:
+                    await ctx.respond("Confirmed...")
+                    await ticket_name.delete()
+                else:
+                    print("Cancelled...")
+                    await ctx.send("You don't have the permission to close this ticket.")
             else:
-                await ctx.send("You don't have the permission to close this ticket.")
-
+                pass
         except Exception as e:
             # Log any errors that occur during ticket closure
             logging.error(f'An error occurred in {self.__class__.__name__}: {e}', exc_info=True)
